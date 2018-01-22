ed4621b9945f503ac9f2e68cc1afdbb1601acbaa
toriten1034 <umitakamaru24@gmail.com>
2018-01-22 10:52:40 +0900

    Initial commit

--- app/src/main/java/com/example/elab/opencv_test/MainActivity.java
+++ app/src/main/java/com/example/elab/opencv_test/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.elab.opencv_test;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mImageView = null;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -15,9 +24,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bmp1 = BitmapFactory.decodeResource(getResources(),R.drawable.sample_image);
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        TextView tv = findViewById(R.id.sample_text);
+        mImageView = findViewById(R.id.imageView1);
+        Mat img1 = new Mat();
+        Mat img2 = new Mat();
+        Utils.bitmapToMat(bmp1,img1);
+        Core.absdiff(img1,new Scalar(255,255,255),img2);
+        Utils.matToBitmap(img2,bmp1);
+        mImageView.setImageBitmap(bmp1);
+        tv.setText("test");
     }
 
     /**
