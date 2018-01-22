8ad4998467731740784c996e51bf7ab4bccd09d6
toriten1034 <umitakamaru24@gmail.com>
2018-01-22 14:16:35 +0900

    Initial commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,17 +4,49 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-extern "C"
-JNIEXPORT jstring
+//#include "OmnidirectionalCamera.hpp"
 
-JNICALL
-Java_com_example_elab_opencv_1test_MainActivity_stringFromJNI(
+int toGray(cv::Mat img, cv::Mat &Gray);
+extern "C" {
+
+JNIEXPORT jstring JNICALL Java_com_example_elab_opencv_1test_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    cv::Mat test(100,100,CV_8UC3);
+    cv::Mat test(100, 100, CV_8UC3);
 
     std::ostringstream oss;
     oss << "rows = " << test.rows << "cols = " << test.cols << std::endl;
     std::string hello = oss.str();
     return env->NewStringUTF(hello.c_str());
 }
+JNIEXPORT jint
+Java_com_example_elab_opencv_1test_MainActivity_ConvertNative(JNIEnv *, jobject, jlong addrRgba,
+                                                              jlong addrGray);
+
+
+JNIEXPORT jint
+Java_com_example_elab_opencv_1test_MainActivity_ConvertNative(JNIEnv *, jobject, jlong addrRgba,
+                                                              jlong addrGray) {
+
+    cv::Mat &mRgb = *(cv::Mat *) addrRgba;
+    cv::Mat &mGray = *(cv::Mat *) addrGray;
+
+    int con;
+    jint ret;
+
+    con = toGray(mRgb, mGray);
+    ret = (jint) con;
+
+    return ret;
+}
+}
+
+int toGray(cv::Mat img, cv::Mat &gray) {
+    cv::cvtColor(img, gray, CV_RGB2GRAY);
+    if (gray.rows == img.rows && gray.cols == img.cols) {
+        return (1);
+    }
+    return (0);
+
+}
+
